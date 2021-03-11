e555e623256d63adbabe6ff578e0b47800f4e4a0
Lauryn Kim <laurynapr99@gmail.com>
2021-03-11 13:13:48 +0900

    refactors with react hooks

--- client/components/map.js
+++ client/components/map.js
@@ -1,37 +1,31 @@
-import React, {Component} from 'react'
-import ReactMapGl from 'react-map-gl'
+import React, {Component, useState} from 'react'
+import ReactMapGl, {Popup} from 'react-map-gl'
 import Geocoder from 'react-mapbox-gl-geocoder'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-//latitude: 40.78343, longitude: -73.96625,
+//latitude: 40.78343, longitude: -73.96625, zoom 11
+const token =
+  'pk.eyJ1IjoibGF1cnluYXByOTkiLCJhIjoiY2trZThhNzRhMDN2NjMwcGVjMHA4bG5kZSJ9.adHA-Pgnztq28O9TKW0SHQ'
+//mapStyle="mapbox://styles/mapbox/streets-v10"
 
-export class Map extends Component {
-  constructor() {
-    super()
-    this.state = {
-      viewport: {
-        latitude: 40.78343,
-        longitude: -73.96625,
-        zoom: 11
-      }
-    }
-  }
+export function Map() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 40.78343,
+    longitude: -73.96625,
+    zoom: 11
+  })
 
-  componentDidMount() {}
-  render() {
-    return (
-      <div>
-        <ReactMapGl
-          {...this.state.viewport}
-          width="100vw"
-          height="100vh"
-          mapboxApiAccessToken="pk.eyJ1IjoibGF1cnluYXByOTkiLCJhIjoiY2trZThhNzRhMDN2NjMwcGVjMHA4bG5kZSJ9.adHA-Pgnztq28O9TKW0SHQ"
-          mapStyle="mapbox://styles/mapbox/streets-v10"
-        />
-      </div>
-    )
-  }
+  return (
+    <ReactMapGl
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      mapboxApiAccessToken={token}
+      mapStyle="mapbox://styles/mapbox/streets-v10"
+    />
+  )
 }
 
 // const mapStateToProps = state => ({
