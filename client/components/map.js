f97c45e4cb1299e989013e7eaa9b5a13429e9cd6
Lauryn Kim <laurynapr99@gmail.com>
2021-03-11 14:19:49 +0900

    adds geocoder

--- client/components/map.js
+++ client/components/map.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react'
+import React, {Component, useState, useRef, useCallback} from 'react'
 import ReactMapGl, {Popup} from 'react-map-gl'
 import Geocoder from 'react-mapbox-gl-geocoder'
 import {connect} from 'react-redux'
@@ -9,6 +9,10 @@ const token =
   'pk.eyJ1IjoibGF1cnluYXByOTkiLCJhIjoiY2trZThhNzRhMDN2NjMwcGVjMHA4bG5kZSJ9.adHA-Pgnztq28O9TKW0SHQ'
 //mapStyle="mapbox://styles/mapbox/streets-v10"
 
+const queryParams = {
+  country: 'us'
+}
+
 export function Map() {
   const [viewport, setViewport] = useState({
     width: '100vw',
@@ -17,14 +21,35 @@ export function Map() {
     longitude: -73.96625,
     zoom: 11
   })
+  const mapRef = useRef()
+
+  //Receives selected item and the viewport to set for the selected item
+  const onSelected = useCallback((nextViewport, item) => {
+    setViewport(nextViewport)
+    console.log('Selected item:', item)
+  })
 
   return (
-    <ReactMapGl
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapboxApiAccessToken={token}
-      mapStyle="mapbox://styles/mapbox/streets-v10"
-    />
+    <div>
+      <Geocoder
+        ref={mapRef}
+        viewport={viewport}
+        onSelected={onSelected}
+        mapboxApiAccessToken={token}
+        hideOnSelect={true}
+        queryParams={queryParams}
+        position="top-right"
+        placeholder="Search for a location!"
+        value=""
+      />
+      <ReactMapGl
+        ref={mapRef}
+        {...viewport}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapboxApiAccessToken={token}
+        mapStyle="mapbox://styles/mapbox/streets-v10"
+      />
+    </div>
   )
 }
 
